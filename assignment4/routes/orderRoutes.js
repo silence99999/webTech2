af2266b8e1b54ad8ff011b77f5755d57858133ac
Amir Amir <ghostqwertzuiop@gmail.com>
2026-02-09 19:28:34 +0500

    fix place order

--- assignment4/routes/orderRoutes.js
+++ assignment4/routes/orderRoutes.js
@@ -3,8 +3,8 @@ const auth = require("../middleware/authMiddleware");
 const role = require("../middleware/roleMiddleware");
 const controller = require("../controllers/orderController");
 
-router.get("/", auth,role("admin"), controller.getAllOrders);
-router.get("/:id", auth,role("admin"), controller.getOrderById);
+router.get("/", auth, controller.getAllOrders);
+router.get("/:id", auth, controller.getOrderById);
 
 router.post("/", auth, role("admin"), controller.createOrder);
 router.put("/:id", auth, role("admin"), controller.updateOrder);
